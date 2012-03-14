14146c887a0b985f1d644f8ec014aa85484fd332
Liu Liu <liuliu.1987@gmail.com>
2012-03-14 03:37:45 -0400

    changed color setting so that it looks correct in dark

--- case.h
+++ case.h
@@ -57,7 +57,7 @@ static void INTERNAL_CATCH_UNIQUE_NAME(__test_case_driver__) (char* __case_name_
 #define REQUIRE_EQ(a, b, err, ...) { \
 if ((a) != (b)) \
 { \
-	printf("\n\t\033[0;31mREQUIRE_EQ\033[0;30m: %s:%d: %s(%lg) != %s(%lg), " err, __FILE__, __LINE__, #a, (double)(a), #b, (double)(b), ##__VA_ARGS__); \
+	printf("\n\t\033[0;31mREQUIRE_EQ\033[0;0m: %s:%d: %s(%lg) != %s(%lg), " err, __FILE__, __LINE__, #a, (double)(a), #b, (double)(b), ##__VA_ARGS__); \
 	ABORT_CASE; \
 } }
 
@@ -66,14 +66,14 @@ int __case_i__; \
 for (__case_i__ = 0; __case_i__ < (len); __case_i__++) \
 	if (((type*)(a))[__case_i__] != ((type*)(b))[__case_i__]) \
 	{ \
-		printf("\n\t\033[0;31mREQUIRE_ARRAY_EQ\033[0;30m: %s:%d: %s[%d](%lg) != %s[%d](%lg), " err, __FILE__, __LINE__, #a, __case_i__, (double)((type*)(a))[__case_i__], #b, __case_i__, (double)((type*)(b))[__case_i__], ##__VA_ARGS__); \
+		printf("\n\t\033[0;31mREQUIRE_ARRAY_EQ\033[0;0m: %s:%d: %s[%d](%lg) != %s[%d](%lg), " err, __FILE__, __LINE__, #a, __case_i__, (double)((type*)(a))[__case_i__], #b, __case_i__, (double)((type*)(b))[__case_i__], ##__VA_ARGS__); \
 		ABORT_CASE; \
 	} }
 
 #define REQUIRE_EQ_WITH_TOLERANCE(a, b, t, err, ...) { \
 if ((double)((a) - (b)) > (t) || (double)((a) - (b)) < -(t)) \
 { \
-	printf("\n\t\033[0;31mREQUIRE_EQ_WITH_TOLERANCE\033[0;30m: %s:%d: %s(%lg) != %s(%lg) | +-%lg, " err, __FILE__, __LINE__, #a, (double)(a), #b, (double)(b), (double)(t), ##__VA_ARGS__); \
+	printf("\n\t\033[0;31mREQUIRE_EQ_WITH_TOLERANCE\033[0;0m: %s:%d: %s(%lg) != %s(%lg) | +-%lg, " err, __FILE__, __LINE__, #a, (double)(a), #b, (double)(b), (double)(t), ##__VA_ARGS__); \
 	ABORT_CASE; \
 } }
 
@@ -82,14 +82,14 @@ int __case_i__; \
 for (__case_i__ = 0; __case_i__ < (len); __case_i__++) \
 	if ((double)(((type*)(a))[__case_i__] - ((type*)(b))[__case_i__]) > (t) || (double)(((type*)(a))[__case_i__] - ((type*)(b))[__case_i__]) < -(t)) \
 	{ \
-		printf("\n\t\033[0;31mREQUIRE_ARRAY_EQ_WITH_TOLERANCE\033[0;30m: %s:%d: %s[%d](%lg) != %s[%d](%lg) | +-%lg, " err, __FILE__, __LINE__, #a, __case_i__, (double)((type*)(a))[__case_i__], #b, __case_i__, (double)((type*)(b))[__case_i__], (double)(t), ##__VA_ARGS__); \
+		printf("\n\t\033[0;31mREQUIRE_ARRAY_EQ_WITH_TOLERANCE\033[0;0m: %s:%d: %s[%d](%lg) != %s[%d](%lg) | +-%lg, " err, __FILE__, __LINE__, #a, __case_i__, (double)((type*)(a))[__case_i__], #b, __case_i__, (double)((type*)(b))[__case_i__], (double)(t), ##__VA_ARGS__); \
 		ABORT_CASE; \
 	} }
 
 #define REQUIRE_NOT_EQ(a, b, err, ...) { \
 if ((a) == (b)) \
 { \
-	printf("\n\t\033[0;31mREQUIRE_NOT_EQ\033[0;30m: %s:%d: %s(%lg) == %s(%lg), " err, __FILE__, __LINE__, #a, (double)(a), #b, (double)(b), ##__VA_ARGS__); \
+	printf("\n\t\033[0;31mREQUIRE_NOT_EQ\033[0;0m: %s:%d: %s(%lg) == %s(%lg), " err, __FILE__, __LINE__, #a, (double)(a), #b, (double)(b), ##__VA_ARGS__); \
 	ABORT_CASE; \
 } }
 
@@ -98,14 +98,14 @@ int __case_i__; \
 for (__case_i__ = 0; __case_i__ < (len); __case_i__++) \
 	if (((type*)(a))[__case_i__] == ((type*)(b))[__case_i__]) \
 	{ \
-		printf("\n\t\033[0;31mREQUIRE_ARRAY_NOT_EQ\033[0;30m: %s:%d: %s[%d](%lg) == %s[%d](%lg), " err, __FILE__, __LINE__, #a, __case_i__, (double)((type*)(a))[__case_i__], #b, __case_i__, (double)((type*)(b))[__case_i__], ##__VA_ARGS__); \
+		printf("\n\t\033[0;31mREQUIRE_ARRAY_NOT_EQ\033[0;0m: %s:%d: %s[%d](%lg) == %s[%d](%lg), " err, __FILE__, __LINE__, #a, __case_i__, (double)((type*)(a))[__case_i__], #b, __case_i__, (double)((type*)(b))[__case_i__], ##__VA_ARGS__); \
 		ABORT_CASE; \
 	} }
 
 #define REQUIRE_NOT_EQ_WITH_TOLERANCE(a, b, t, err, ...) { \
 if ((double)((a) - (b)) <= (t) && (double)((a) - (b)) >= -(t)) \
 { \
-	printf("\n\t\033[0;31mREQUIRE_NOT_EQ_WITH_TLERANCE\033[0;30m: %s:%d: %s(%lg) == %s(%lg) | +-%lg, " err, __FILE__, __LINE__, #a, (double)(a), #b, (double)(b), (double)(t), ##__VA_ARGS__); \
+	printf("\n\t\033[0;31mREQUIRE_NOT_EQ_WITH_TLERANCE\033[0;0m: %s:%d: %s(%lg) == %s(%lg) | +-%lg, " err, __FILE__, __LINE__, #a, (double)(a), #b, (double)(b), (double)(t), ##__VA_ARGS__); \
 	ABORT_CASE; \
 } }
 
@@ -114,7 +114,7 @@ int __case_i__; \
 for (__case_i__ = 0; __case_i__ < (len); __case_i__++) \
 	if ((double)(((type*)(a))[__case_i__] - ((type*)(b))[__case_i__]) <= (t) && (double)(((type*)(a))[__case_i__] - ((type*)(b))[__case_i__]) >= -(t)) \
 	{ \
-		printf("\n\t\033[0;31mREQUIRE_ARRAY_NOT_EQ_WITH_TOLERANCE\033[0;30m: %s:%d: %s[%d](%lg) == %s[%d](%lg) | +-%lg, " err, __FILE__, __LINE__, #a, __case_i__, (double)((type*)(a))[__case_i__], #b, __case_i__, (double)((type*)(b))[__case_i__], (double)(t), ##__VA_ARGS__); \
+		printf("\n\t\033[0;31mREQUIRE_ARRAY_NOT_EQ_WITH_TOLERANCE\033[0;0m: %s:%d: %s[%d](%lg) == %s[%d](%lg) | +-%lg, " err, __FILE__, __LINE__, #a, __case_i__, (double)((type*)(a))[__case_i__], #b, __case_i__, (double)((type*)(b))[__case_i__], (double)(t), ##__VA_ARGS__); \
 		ABORT_CASE; \
 	} }
 
